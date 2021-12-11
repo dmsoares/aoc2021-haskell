3cfa9e061cf9686376fb2d6cef27c537d642722b
Decio Soares <decio.msoares@gmail.com>
2021-12-11 10:51:43 +0000

    Add day2-p2 solution.

--- day2.hs
+++ day2.hs
@@ -8,9 +8,13 @@ main = do
   content <- readFile "day2-input"
   let linesOfFiles = lines content
   print (part1 linesOfFiles)
+  print (part2 linesOfFiles)
 
 part1 :: [String] -> Int
 part1 =  uncurry (*) . foldl nextPos (0, 0)
+
+part2 :: [String] -> Int
+part2 =  (\(x, y, aim) -> x * y) . foldl nextPos' (0, 0, 0)
 
 nextPos :: (Int, Int) -> String -> (Int, Int)
 nextPos (x, y) str =
@@ -18,3 +22,10 @@ nextPos (x, y) str =
     ["forward", n] -> (x + read n, y)
     ["down", n]    -> (x, y + read n)
     ["up", n]      -> (x, y - read n)
+
+nextPos' :: (Int, Int, Int) -> String -> (Int, Int, Int)
+nextPos' (x, y, aim) str =
+  case words str of
+    ["forward", n] -> (x + read n, read n * aim + y, aim)
+    ["down", n]    -> (x, y, aim + read n)
+    ["up", n]      -> (x, y, aim - read n)
