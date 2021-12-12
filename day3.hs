8cd43762522d5c430b19c69ff3a77bee4e85eb8e
Decio Soares <decio.msoares@gmail.com>
2021-12-12 17:08:57 +0000

    Add day3-p2 solution.

--- day3.hs
+++ day3.hs
@@ -7,10 +7,11 @@ main = do
   content <- readFile "day3-input"
   let linesOfFile = lines content
   print $ part1 linesOfFile
+  print $ part2 linesOfFile
 
 gammaPartial :: String -> Bool
 gammaPartial str =
-  n >= length str `div` 2
+  n >= (length str `div` 2) + (length str `mod` 2)
   where
     n = foldr (\a b -> read [a] + b) 0 str
 
@@ -22,6 +23,29 @@ binToDec = fst . foldr f (0, 0)
   where f True (n, p) = (n + 2 ^ p, p + 1)
         f _ (n, p)    = (n, p + 1)
 
+strToBin :: String -> [Bool]
+strToBin = map (== '1')
+
+oxyGen :: [String] -> [String]
+oxyGen = go 0
+  where
+    go _ [] = []
+    go _ [l] = [l]
+    go i m =
+      if gammaPartial . (!! i) . transpose $ m
+      then go (i + 1) $ filter ((== '1') . (!! i)) m
+      else go (i + 1) $ filter ((== '0') . (!! i)) m
+
+co2Scrub :: [String] -> [String]
+co2Scrub = go 0
+  where
+    go _ [] = []
+    go _ [l] = [l]
+    go i m =
+      if not . gammaPartial . (!! i) . transpose $ m
+      then go (i + 1) $ filter ((== '1') . (!! i)) m
+      else go (i + 1) $ filter ((== '0') . (!! i)) m
+
 part1 :: [String] -> Int
 part1 lines =
   let transposed = transpose lines
@@ -30,4 +54,7 @@ part1 lines =
   in
     binToDec gamma * binToDec epsilon
 
-sample = "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010"
+part2 :: [String] -> Int
+part2 lines =
+  binToDec (strToBin . head . oxyGen $ lines) *
+  binToDec (strToBin . head . co2Scrub $ lines)
