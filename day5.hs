a1e1c2301eb6ddd27778bcd2e2b9d456fd53c383
Decio Soares <decio.msoares@gmail.com>
2021-12-20 09:41:48 +0000

    Add day5-p2 solution.

--- day5.hs
+++ day5.hs
@@ -3,11 +3,14 @@ module Day5 where
 main :: IO ()
 main = do
   contents <- readFile "day5-input"
-  print . part1 . pointsOfLines . onlyOrto . parseCoords . lines $ input
-  print . part1 . pointsOfLines . onlyOrto . parseCoords . lines $ contents
+  print . concatMap pointsFromLine . onlyOrto . parseCoords . lines $ input
+  print . length . concatMap pointsFromLine . onlyOrto . parseCoords . lines $ input
+  print . part1 . concatMap pointsFromLine . parseCoords . lines $ contents
 
 input = "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2\n"
 input' = "0,9 -> 5,9\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n0,9 -> 2,9\n3,4 -> 1,4\n"
+
+type Point = (Int, Int)
 
 split :: String -> String -> [String]
 split sep s = go sep s [[]]
@@ -20,37 +23,32 @@ split sep s = go sep s [[]]
       else go sep s' ((head acc ++ [c]) : tail acc)
     startsWith s = (== s) . take (length s)
 
-strToPair :: String -> (Int, Int)
+strToPair :: String -> Point
 strToPair s = (read a, read b)
   where
     r = split "," s
     a = head r
     b = head . tail $ r
 
-parseCoords :: [String] -> [((Int, Int), (Int, Int))]
+parseCoords :: [String] -> [(Point, Point)]
 parseCoords = map ((\[a, b] -> (a, b)) . map strToPair . split " -> ")
 
-alignCoords :: [((Int, Int), (Int, Int))] -> [((Int, Int), (Int, Int))]
-alignCoords = map (\((a, b), (c, d)) -> ((a, c), (b, d)))
-
-orderPair :: (Int, Int) -> (Int, Int)
-orderPair (x, y) =
-  if x >= y
-  then (y, x)
-  else (x, y)
-
-orderPoints :: ((Int, Int), (Int, Int)) -> ((Int, Int), (Int, Int))
-orderPoints (a, b) = (orderPair a, orderPair b)
-
-pointsOfLines :: [((Int, Int), (Int, Int))] -> [(Int, Int)]
-pointsOfLines = concatMap (f . orderPoints) . alignCoords
+pointsFromLine :: (Point, Point) -> [Point]
+pointsFromLine (pa, pb) = go pa pb []
   where
-    f ((x, y), (w, z)) = [(a, b) | a <- [x..y], b <- [w..z]]
+    go a@(xa, ya) b@(xb, yb) acc
+      | a == b = a : acc
+      | xa == xb = go (xa, ya') b (a : acc)
+      | ya == yb = go (xa', ya) b (a : acc)
+      | otherwise = go (xa', ya') b (a : acc)
+      where
+        xa' = if xa > xb then xa - 1 else xa + 1
+        ya' = if ya > yb then ya - 1 else ya + 1
 
-onlyOrto :: [((Int, Int), (Int, Int))] -> [((Int, Int), (Int, Int))]
+onlyOrto :: [(Point, Point)] -> [(Point, Point)]
 onlyOrto = filter (\((a, b), (c, d)) -> a == c || b == d)
 
-part1 :: [(Int, Int)] -> Int
+part1 :: [Point] -> Int
 part1 ps = go ps 0 []
   where
     go [] n _ = n
